a9d68ffa0668fee9287ef161da03d2225ebf2f18
simple256 <michigan96@yandex.ru>
2020-05-23 15:21:45 +0500

    Подключение ssl сертификата

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import * as fs from 'fs';
+import * as https from 'https';
 import * as mongoose from 'mongoose';
 import { express as expressCfg, router } from './config';
 require('dotenv/config');
@@ -7,13 +9,23 @@ const app = express();
 expressCfg(app);
 router(app);
 
+/**
+ * Для работы приложения требуется разместить файлы с сертификатом и ключом
+ *  в папку sslCert
+ */
+const privateKey = fs.readFileSync('sslCert/certificate.key', 'utf8');
+const certificate = fs.readFileSync('sslCert/certificate.cer', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+
 mongoose
   .connect(process.env.DB_CONNECTION, { useUnifiedTopology: true, useNewUrlParser: true }, () => {
     console.log('connected to MongoDB');
   })
-  .then(() =>
-    app.listen(process.env.APP_PORT, () => {
-      console.log(`Listening on ${process.env.APP_PORT}...`);
-    }),
-  )
+  .then(() => {
+    httpsServer.listen(443, () => {
+      console.log(`Listening on 443...`);
+    });
+  })
   .catch(() => console.log('Connection failed'));
