918480fd41871fe0a644251f7dae0c30030edc53
simple256 <michigan96@yandex.ru>
2020-05-31 22:36:00 +0500

    Fix Routes

--- config/router.ts
+++ config/router.ts
@@ -14,28 +14,28 @@ export = (app) => {
   /**
    * Actions requests
    */
-  app.get('/actions', checkToken, isAuth, attachCurrentUser, actionsController.getAll);
-  app.post('/actions', checkToken, isAuth, attachCurrentUser, actionsController.create);
-  app.put('/action/:id', checkToken, isAuth, attachCurrentUser, actionsController.update);
-  app.delete('/action/:id', checkToken, isAuth, attachCurrentUser, actionsController.remove);
+  app.get('/api/actions', checkToken, isAuth, attachCurrentUser, actionsController.getAll);
+  app.post('/api/actions', checkToken, isAuth, attachCurrentUser, actionsController.create);
+  app.put('/api/action/:id', checkToken, isAuth, attachCurrentUser, actionsController.update);
+  app.delete('/api/action/:id', checkToken, isAuth, attachCurrentUser, actionsController.remove);
 
   /**
    * Actors requests
    */
-  app.get('/actors', checkToken, isAuth, attachCurrentUser, actorsController.getAll);
-  app.post('/actors', checkToken, isAuth, attachCurrentUser, actorsController.create);
-  app.put('/actor/:id', checkToken, isAuth, attachCurrentUser, actorsController.update);
-  app.delete('/actor/:id', checkToken, isAuth, attachCurrentUser, actorsController.remove);
+  app.get('/api/actors', checkToken, isAuth, attachCurrentUser, actorsController.getAll);
+  app.post('/api/actors', checkToken, isAuth, attachCurrentUser, actorsController.create);
+  app.put('/api/actor/:id', checkToken, isAuth, attachCurrentUser, actorsController.update);
+  app.delete('/api/actor/:id', checkToken, isAuth, attachCurrentUser, actorsController.remove);
 
   /**
    * Auth requests
    */
-  app.post('/signin', authController.signIn);
+  app.post('/api/signin', authController.signIn);
   // {
   //   "email": "aaaaa2222a@mail",
   //   "password": "123123123123123123"
   // }
-  app.post('/register', authController.register);
+  app.post('/api/register', authController.register);
   // {
   //   "email": "aaaaa2222a@mail",
   //   "password": "123123123123123123"
@@ -44,36 +44,36 @@ export = (app) => {
   /**
    * Objects requests
    */
-  app.get('/objects', checkToken, isAuth, attachCurrentUser, objectsController.getAll);
-  app.post('/objects', checkToken, isAuth, attachCurrentUser, objectsController.create);
-  app.put('/object/:id', checkToken, isAuth, attachCurrentUser, objectsController.update);
-  app.delete('/object/:id', checkToken, isAuth, attachCurrentUser, objectsController.remove);
+  app.get('/api/objects', checkToken, isAuth, attachCurrentUser, objectsController.getAll);
+  app.post('/api/objects', checkToken, isAuth, attachCurrentUser, objectsController.create);
+  app.put('/api/object/:id', checkToken, isAuth, attachCurrentUser, objectsController.update);
+  app.delete('/api/object/:id', checkToken, isAuth, attachCurrentUser, objectsController.remove);
 
   /**
    * Projects requests
    */
-  app.get('/projects', checkToken, isAuth, attachCurrentUser, projectsController.getAll);
-  app.post('/projects', checkToken, isAuth, attachCurrentUser, projectsController.create);
-  app.put('/project/:id', checkToken, isAuth, attachCurrentUser, projectsController.update);
-  app.delete('/project/:id', checkToken, isAuth, attachCurrentUser, projectsController.remove);
+  app.get('/api/projects', checkToken, isAuth, attachCurrentUser, projectsController.getAll);
+  app.post('/api/projects', checkToken, isAuth, attachCurrentUser, projectsController.create);
+  app.put('/api/project/:id', checkToken, isAuth, attachCurrentUser, projectsController.update);
+  app.delete('/api/project/:id', checkToken, isAuth, attachCurrentUser, projectsController.remove);
 
   /**
    * Stages requests
    */
-  // app.get('/stages', stagesController.getAll);
-  app.get('/stage/:id', checkToken, isAuth, attachCurrentUser, stagesController.get);
-  app.post('/stages', checkToken, isAuth, attachCurrentUser, stagesController.create);
-  app.put('/stage/:id', checkToken, isAuth, attachCurrentUser, stagesController.update);
-  app.delete('/stage/:id', checkToken, isAuth, attachCurrentUser, stagesController.remove);
+  // app.get('/api/stages', stagesController.getAll);
+  app.get('/api/stage/:id', checkToken, isAuth, attachCurrentUser, stagesController.get);
+  app.post('/api/stages', checkToken, isAuth, attachCurrentUser, stagesController.create);
+  app.put('/api/stage/:id', checkToken, isAuth, attachCurrentUser, stagesController.update);
+  app.delete('/api/stage/:id', checkToken, isAuth, attachCurrentUser, stagesController.remove);
 
   /**
    * User request
    */
-  app.put('/user/:id', checkToken, isAuth, attachCurrentUser, userController.update);
+  app.put('/api/user/:id', checkToken, isAuth, attachCurrentUser, userController.update);
 
   /**
    * projectCategories request
    */
-  app.get('/projectCategories', projectCategoriesController.getFirstLevel);
-  app.post('/projectCategories', projectCategoriesController.create);
+  app.get('/api/projectCategories', projectCategoriesController.getFirstLevel);
+  app.post('/api/projectCategories', projectCategoriesController.create);
 };
